8bcba0a096194226175bd7bee02723b5d9537429
Bibhas Ghoshal <bibhas@shoppre.com>
2020-09-19 13:23:14 +0530

    code align and spell checked

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-window.addEventListener("scroll", function(){
-console.log('sticky')
-var header = document.querySelector("header");
-header.classList.toggle("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function () {
+    console.log('sticky')
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 })
